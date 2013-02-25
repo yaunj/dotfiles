3284588becb7392dd83735dadd01cea17d709d8c
Rune Hammersland <rune@hammersland.net>
2013-02-25 23:49:15 +0100

    Support dotfiles in directories.
    This change adds support for files in directories. When installing
    symlinks, only symlinks to files will be created, so files within
    $HOME/.config can be versioned withoug versioning the whole directory.

--- install.sh
+++ install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 cd $(dirname $0)
+GITREPO="${PWD}"
+
+check_link()
+{
+    target=${1}
+    link_name=${2}
+
+    if [ -e "${link_name}" ]; then
+        if [ -L "${link_name}" ]; then
+            # Suppress error if link_name already points to destination
+            end=$(python -c "import os; print os.path.realpath('${link_name}');")
+            if [ "${end}" != "${target}" ]; then
+                echo "[FAIL] ${link_name} is a link and points elsewhere." >&2
+            fi
+        else
+            echo "[FAIL] ${link_name} is an existing file" >&2
+        fi
+
+        return 1
+    else
+        return 0
+    fi
+}
 
 for f in *; do
     [[ "${f}" = "${0}" ]] && continue
@@ -7,18 +30,23 @@ for f in *; do
     link=${HOME}/.${f}
     this=${PWD}/${f}
 
-    if [[ -e "${link}" ]]; then
-        if [[ -L "${link}" ]]; then
-            end=$(python -c "import os; print os.path.realpath('${link}');")
-
-            if [[ "${end}" != "${this}" ]]; then
-                echo "[FAIL] ${link} points elsewhere. Fix it yourself." >&2
-            fi
-        else
-            echo "[FAIL] .${f} exists in \$HOME. Symlink it yourself." >&2
+    if [ -f "${this}" ]; then
+        # This is one of the toplevel files in the repository.
+        # Try to symlink from $HOME
+        if check_link "${this}" "${link}"; then
+            ln -s "${this##${HOME}/}" "${link}"
+            echo "[ OK ] Created $link" >&2
         fi
-    else
-        ln -s "${this}" "${link}"
-        echo "[ OK ] Created $link" >&2
+    elif [ -d "${this}" ]; then
+        # This is a directory at the toplevel of the repository
+        # Find all files, and symlink just the files
+        for subfile in $(find "${this}" -type f); do
+            link="${HOME}/.${subfile##${GITREPO}/}"
+            if check_link "${subfile}" "${link}"; then
+                mkdir -p "$(dirname "${subfile}")" 2>/dev/null
+                ln -s "${subfile}" "${link}"
+                echo "[ OK ] Created $link" >&2
+            fi
+        done
     fi
 done
